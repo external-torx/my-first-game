11cf7748da3f5e1c936901322cd3c8c3adc1bcaa
Hallie Parry <halloran.parry@gmail.com>
2015-11-21 18:30:53 -0800

    A build!

---
+++ demo-html/src/com/packtpub/libgdx/demo/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.packtpub.libgdx.demo.client;
+
+import com.packtpub.libgdx.demo.MyDemo;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new MyDemo();
+	}
+}
